dd2c85d2f930a82966c26173120c85fe4938d3d0
Roberto Wagner <roberto-wagner.mendes@serpro.gov.br>
2024-07-19 21:30:02 -0300

    Adding dist

---
+++ dist/assets/calcite-scrim.b8319447.js
@@ -0,0 +1,5 @@
+import{S as t,d as m}from"./scrim.51d512d2.js";import"./index.221827cd.js";import"./loader.d1e283c7.js";import"./guid.ae96513b.js";/*!
+ * All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+ * See https://github.com/Esri/calcite-components/blob/master/LICENSE.md for details.
+ * v1.0.0-beta.97
+ */const n=t,s=m;export{n as CalciteScrim,s as defineCustomElement};
