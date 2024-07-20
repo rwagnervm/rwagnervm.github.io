8b1db0df9d6b1a7a067b57e3b2a6c71446852729
Roberto Wagner <roberto-wagner.mendes@serpro.gov.br>
2024-07-19 22:04:20 -0300

    Adding dist

---
+++ assets/jsonContext.8a25d8b2.js
@@ -0,0 +1,1 @@
+import{bX as r,bW as e}from"./index.9560fde2.js";function l(t){return{origin:"portal-item",url:r(t.itemUrl),portal:t.portal||e.getDefault(),portalItem:t,readResourcePaths:[]}}function a(t){return{origin:"portal-item",messages:[],writtenProperties:[],url:t.itemUrl?r(t.itemUrl):null,portal:t.portal||e.getDefault(),portalItem:t}}export{l as e,a as o};
---
+++ assets/requestImageUtils.91aed7c5.js
@@ -0,0 +1,1 @@
+import{U as n}from"./index.9560fde2.js";async function r(t,a){const{data:e}=await n(t,{responseType:"image",...a});return e}export{r as t};
