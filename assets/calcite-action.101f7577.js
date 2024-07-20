e5689f76f250f0686a39580adcb2b188414c298b
Roberto Wagner <roberto-wagner.mendes@serpro.gov.br>
2024-07-19 21:40:30 -0300

    Adding dist

--- assets/calcite-action.101f7577.js
+++
@@ -1,5 +0,0 @@
-import{A as t,d as o}from"./action.033e9799.js";import"./index.221827cd.js";import"./observers.440ad120.js";import"./interactive.d71a7a54.js";import"./icon.781cca93.js";import"./loader.d1e283c7.js";import"./guid.ae96513b.js";/*!
- * All material copyright ESRI, All Rights Reserved, unless otherwise specified.
- * See https://github.com/Esri/calcite-components/blob/master/LICENSE.md for details.
- * v1.0.0-beta.97
- */const c=t,a=o;export{c as CalciteAction,a as defineCustomElement};
